0c5828b8e17cb0abfc8720e2982928e3c4ace798
NikitaGoncharick <93119559+NikitaGoncharick@users.noreply.github.com>
2025-10-12 16:15:28 +0200

    Project Files

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,20 @@
+from tempfile import template
+
 from fastapi import FastAPI
 import uvicorn
-
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
 
 
 
 app = FastAPI()
 
 
+template = Jinja2Templates(directory="../frontend/templates")
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request: Request):
+    return template.TemplateResponse("home.html", {"request": request})
 
 
 
